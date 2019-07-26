bf83bd96fe7ee6ae70e0634110c7d89312cc48bd
Revin Patel <rsp722@gmail.com>
2019-07-26 20:35:30 +0000

    Created a form for users to input places.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,12 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    # @places = Place.order(:id).paginate(:page => params[:page], :per_page => 10)
+    @places = Place.order("name").page(params[:page]).per_page(10)
+  end
+
+  def new
+    @place = Place.new
   end
 
 end
