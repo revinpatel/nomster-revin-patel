80858e467523de336ca6d9cdc5e49c217c256633
Revin Patel <rsp722@gmail.com>
2019-07-28 06:52:16 +0000

    Added function for geocoding addresses.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  geocoded_by :address
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :address, presence: true
+  after_validation :geocode
+  validates :description, presence: true
 end
